1aa8950eaa70181b8e387c3e926d74151c96139b
smalaca <ab13.krakow@gmail.com>
2018-10-20 19:27:51 +0200

    WordCounter - should recognize when word is accepted

--- testdrivendevelopment/src/main/java/com/smalaca/wordcounter/WordCounter.java
+++ testdrivendevelopment/src/main/java/com/smalaca/wordcounter/WordCounter.java
@@ -1,11 +1,13 @@
 package com.smalaca.wordcounter;
 
 public class WordCounter {
+    private boolean accepted;
+
     public int count(String word) {
-        return 0;
+        return accepted ? 1 : 0;
     }
 
     public void accept(String word) {
-
+        accepted = true;
     }
 }
