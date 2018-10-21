5e6dac921204d87ba9e48eaf805084478f851025
smalaca <ab13.krakow@gmail.com>
2018-10-21 14:28:53 +0200

    TestsSuites - not adding TestScenario when author has no privileges

--- testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
+++ testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
@@ -3,13 +3,17 @@ package com.smalaca.testsuite;
 import java.util.Map;
 
 class TestsSuite {
+    private final AuthorAccess authorAccess;
     private final Map<Author, TestScenario> testScenarios;
 
     TestsSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios) {
+        this.authorAccess = authorAccess;
         this.testScenarios = testScenarios;
     }
 
     void add(TestScenario testScenario, Author author) {
-        testScenarios.put(author, testScenario);
+        if (authorAccess.hasPrivileges(author)) {
+            testScenarios.put(author, testScenario);
+        }
     }
 }
