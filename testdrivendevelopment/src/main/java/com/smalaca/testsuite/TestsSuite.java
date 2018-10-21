4ef82437440f2dc7ef539dab4ca20456b670a570
smalaca <ab13.krakow@gmail.com>
2018-10-21 14:19:01 +0200

    TestsSuites - remove useless TestsSuite attributes

--- testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
+++ testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
@@ -3,13 +3,9 @@ package com.smalaca.testsuite;
 import java.util.Map;
 
 class TestsSuite {
-    private final TestScenarioValidator validator;
-    private final AuthorAccess authorAccess;
     private final Map<Author, TestScenario> testScenarios;
 
     TestsSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios) {
-        this.validator = validator;
-        this.authorAccess = authorAccess;
         this.testScenarios = testScenarios;
     }
 
