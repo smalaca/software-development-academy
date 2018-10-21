18108b61fc2f8c12ab1044c5298e4c8146644e51
smalaca <ab13.krakow@gmail.com>
2018-10-21 14:45:26 +0200

    TestsSuites - won't add TestScenario when is incorrect

--- testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
+++ testdrivendevelopment/src/main/java/com/smalaca/testsuite/TestsSuite.java
@@ -3,16 +3,18 @@ package com.smalaca.testsuite;
 import java.util.Map;
 
 class TestsSuite {
+    private final TestScenarioValidator validator;
     private final AuthorAccess authorAccess;
     private final Map<Author, TestScenario> testScenarios;
 
     TestsSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios) {
+        this.validator = validator;
         this.authorAccess = authorAccess;
         this.testScenarios = testScenarios;
     }
 
     void add(TestScenario testScenario, Author author) {
-        if (authorAccess.hasPrivileges(author)) {
+        if (authorAccess.hasPrivileges(author) && validator.isCorrect(testScenario)) {
             testScenarios.put(author, testScenario);
         }
     }
